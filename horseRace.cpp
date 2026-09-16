4c129bfc79f8c40161247cf412b0715b412550ca
fish-2121 <hazelgrove2007@gmail.com>
2026-09-16 16:49:25 -0400

    function

--- horseRace.cpp
+++ horseRace.cpp
@@ -7,10 +7,18 @@ const int horsesAmount = 5;
 
 main() {
     int horses[] {0, 0, 0, 0, 0};
-
+    printPositions(horses);
+    
     return 0;
 }
 
 void printPositons(int positionNums[]) {
-    
+    for (int i = 0; i < horsesAmount; i++) {
+        int arr[winPos] = {'*', '*', '*', '*', '*', '*', '*', '*', '*', '*'};
+        arr[positionNums[i]] = i;
+        for (int ii = 0; ii < winPos; ii++) {
+            std::out << arr[ii];
+        }
+        std::out << std::endl;
+    }
 }
